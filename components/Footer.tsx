e2ce9517af280070571ce33b3ac037409b35f3ef
ezher-memeti <134104823+ezher-memeti@users.noreply.github.com>
2026-05-05 17:39:50 +0300

    mail update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,7 +14,7 @@ const linkGroups = [
     title: "Kurumsal",
     links: [
       { label: "Hakkında", href: "/hakkinda" },
-      { label: "İletişim", href: "mailto:destek@iyilikatlasi.com" },
+      { label: "İletişim", href: "mailto:info@iyilikatlasi.com.tr" },
     ],
   },
   {
